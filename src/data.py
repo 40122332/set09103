2fbf82929885c828a9a836ec5b67979dc9ad2c22
40122332 <40122332@live.napier.ac.uk>
2015-10-20 13:09:13 +0000

    Remove unneeded files from repo

--- src/data.py
+++
@@ -1,7 +0,0 @@
-from flask import g
-
-def add_data():
-  g.db.execute('insert into entries (name, size, fur_type, ear_type, origin,\
-  colour, uses) values (?,?,?,?,?,?,?)',["Alaska", "small", "short", "lopped",\
-  "Germany", "Black", "Pet"])
-  g.db.commit()
